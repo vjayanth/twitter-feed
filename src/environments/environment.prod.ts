a1f0df268108be0df2e8f9ef5917fb2184b583d1
jayanth <vjayanth1291@gmail.com>
2017-10-12 22:10:06 +0530

    added prod constants

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
 export const environment = {
-  production: true
+  production: true,
+    firebase: {
+    apiKey: "AIzaSyBAOhuAXmM9MKKDGo5B0lanr8ag4KtXe-s",
+    authDomain: "twitter-feed-3e51d.firebaseapp.com",
+    databaseURL: "https://twitter-feed-3e51d.firebaseio.com",
+    projectId: "twitter-feed-3e51d",
+    storageBucket: "twitter-feed-3e51d.appspot.com",
+    messagingSenderId: "835823728731"
+  }
 };
